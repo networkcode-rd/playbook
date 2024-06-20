e7abbd3ef7ce2e96c4e6d0b42d4be95471ff6fa5
ratnavo dutta <ratnavo.dutta.official@gmail.com>
2024-06-20 16:43:32 +0530

    Updating PS Repo

--- Azure PowerShellv.1/Azure PowerShellv.0/2 c Get VNet Info.ps1
+++
@@ -1,7 +0,0 @@
-$networkName="app-network"
-$resourceGroup="app-grp"
-
-$VirtualNetwork=Get-AzVirtualNetwork -Name $networkName -ResourceGroupName $resourceGroup
-
-Write-Host $VirtualNetwork.AddressSpace.AddressPrefixes
-Write-Host $VirtualNetwork.Location
--- Azure PowerShellv.1/Azure PowerShellv.0/5 Creating Std Public IP.ps1
+++
@@ -1,6 +0,0 @@
-$resourceGroup="app-grp"
-$location="North Europe"
-$publicIPAddress="app-ip"
-
-New-AzPublicIpAddress -Name $publicIPAddress -ResourceGroupName $resourceGroup `
--Location $location -AllocationMethod Static
