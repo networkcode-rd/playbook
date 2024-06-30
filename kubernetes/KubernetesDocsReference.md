17528ea90ef7965d7fd83e32faa8dbb4df16b7ca
networkcode-admin <ratnavo.dutta.official@gmail.com>
2024-06-30 13:15:40 +0530

    updating the .sh file and .txt file

---
+++ kubernetes/KubernetesDocsReference.md
@@ -0,0 +1,9 @@
+## Public Documentation Reference
+
+Link-1 https://kubernetes.io/docs/setup/  
+<br>
+Link-2:https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands
+<br>
+Link-3: https://kubernetes.io/docs/reference/kubectl/quick-reference/
+<br>
+Link-4: https://kubernetes.io/docs/reference/kubectl/
--- kubernetes/azurekubenet.sh
+++ kubernetes/azurekubenet.sh
@@ -42,10 +42,55 @@ kubectl get pod -n customnamespacerd  -o wide
 
 #To get into a NODE from a POD:
 #==============================================
-kubectl debug node/aks-agentpool-20806549-vmss000000 -it --image=mcr.microsoft.com/dotnet/runtime-deps:6.0
-Creating debugging pod node-debugger-aks-agentpool-20806549-vmss000000-k95zk with container debugger on node aks-agentpool-20806549-vmss000000.
-If you don't see a command prompt, try pressing enter.
-root@aks-agentpool-20806549-vmss000000:/# chroot /host
+#kubectl debug node/aks-agentpool-20806549-vmss000000 -it --image=mcr.microsoft.com/dotnet/runtime-deps:6.0
+#Creating debugging pod node-debugger-aks-agentpool-20806549-vmss000000-k95zk with container debugger on node aks-agentpool-20806549-vmss000000.
+#If you don't see a command prompt, try pressing enter.
+#root@aks-agentpool-20806549-vmss000000:/# chroot /host
 # 
 # apt-get update && apt-get install -i iproute2 net-tools
 #==============================================
+
+#To check the status of an existing/newly created PoD on a specific namespace:
+kubectl get all -n customnamespacerd
+kubectl get all -n customnamespacerd -o wide
+
+# Deleting a specific PoD from namespace:
+kubectl delete pod superman -n customnamespacerd
+
+
+# To recreate the PoD but with specific service or running a specific port and deployment service:
+kubectl create deployment supermanpod -n customnamespacerd --image=nginx --port=80 
+
+#check status:
+kubectl get all -n customnamespacerd
+
+# Creating replicas
+kubectl scale deployment supermanpod -n customnamespacerd --replicas=2
+
+
+#Using Declarative approach of reading data in yaml format, use:
+kubectl get pod -n customnamespacerd -o yaml
+
+# Internal Cluster Service:
+kubectl expose deployment.apps/supermanpod -n customnamespacerd
+
+#Check status: 
+kubectl get all -n customnamespacerd
+
+# Getting more information of the service use "describe":
+kubectl describe service/supermanpod -n customnamespacerd
+
+
+#Creating another LB service public facing and then check status:
+kubectl expose deployment.apps/supermanpod --name batman -n customnamespacerd --type=LoadBalance
+
+#Check status: 
+kubectl get all -n customnamespacerd
+
+
+#Get all Kube system information:
+kubectl get all -n kube-system
+
+#Getting information on namespace and its respective service in a nutshell:
+kubectl describe svc -n customnamespacerd
+kubectl describe svc "podname" -n customnamespacerd
