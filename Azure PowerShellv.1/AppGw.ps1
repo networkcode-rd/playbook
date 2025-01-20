9465268d11147346dd04460e734709b00c8f9607
networkcode-admin <ratnavo.dutta.official@gmail.com>
2025-01-20 10:21:31 +0530

    updating AppGw file

--- Azure PowerShellv.1/AppGw.ps1
+++ Azure PowerShellv.1/AppGw.ps1
@@ -8,17 +8,16 @@ Stop-AzApplicationGateway
 
 #========================= Reference https://learn.microsoft.com/en-us/powershell/module/az.network/stop-azapplicationgateway?view=azps-12.5.0 ========================
 
-    $AppGw = Get-AzApplicationGateway -Name AppGwv2 -ResourceGroupName Application-testing
-    Stop-AzApplicationGateway -ApplicationGateway $AppGw
+$AppGw = Get-AzApplicationGateway -Name AppGwv2 -ResourceGroupName Application-testing
+Stop-AzApplicationGateway -ApplicationGateway $AppGw
 
 
+#========================== Start AppGw ==================================================
 
-    #========================== Start AppGw ==================================================
-
-    Start-AzApplicationGateway
-     -ApplicationGateway <PSApplicationGateway>
-     [-DefaultProfile <IAzureContextContainer>]
-     [<CommonParameters>]
+Start-AzApplicationGateway
+-ApplicationGateway <PSApplicationGateway>
+[-DefaultProfile <IAzureContextContainer>]
+[<CommonParameters>]
 
 $AppGw = Get-AzApplicationGateway -Name Test -ResourceGroupName Appgwtest
 Start-AzApplicationGateway -ApplicationGateway $AppGw
