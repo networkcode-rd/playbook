6528a582e22ce0ddf35e355bc536485493fed95e
Ratnavo Dutta Work-seperate-profile <ratnavo.dutta.official@gmail.com>
2025-05-19 12:38:54 +0530

    learning python on list

--- python-database/lists.py
+++ python-database/lists.py
@@ -14,3 +14,15 @@ print(random.choice(friends))
 #2nd Option
 random_index = random.randint(0,4)
 print(friends[random_index])
+
+#Long list of list items
+states_N_UT_of_india = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal", "Andaman and Nicobar Islands", "Chandigarh", "Dadra and Nagar Haveli and Daman and Diu", "Delhi", "Jammu and Kashmir", "Ladakh", "Lakshadweep", "Puducherry"]
+print(len(states_N_UT_of_india))
+print(states_N_UT_of_india[35])
+
+
+# Creating Nested List
+fruits = ["Cherry", "Apple", "Pear"]
+veg = ["Cucumber", "Kale", "Spinnach"]
+grocery_item = [fruits,veg]
+print(grocery_item)
