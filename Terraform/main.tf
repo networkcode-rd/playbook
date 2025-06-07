d0dc1c48e54bd728f27ee25a29904b775c75d845
networkcode-rd <ratnavo.dutta.official@gmail.com>
2025-06-08 00:18:46 +0530

    working on terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "naming-resource-1" {
     filename = "${path.module}/resource-1.txt"
-    content  = "Created a new terraform file."
+    content  = "Created a new terraform file. Now updating the file."
 }
