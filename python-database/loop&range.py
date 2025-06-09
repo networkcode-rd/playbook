105039b09fbb2d2116cb84fd9b6777fbd2af4017
networkcode-rd <ratnavo.dutta.official@gmail.com>
2025-06-09 16:05:35 +0530

    working with pything loops and range

---
+++ python-database/loop&range.py
@@ -0,0 +1,49 @@
+# # student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+# # # # print(range(1, 10))
+# # #
+# # # # total_exam_score = sum(student_scores)
+# # # # print(total_exam_score)
+# # #
+# # # # sum = 0
+# # # # for score in student_scores:
+# # # #     sum += score
+# # # #
+# # # # print(sum)
+# # #
+# # # print(max(student_scores))
+# #
+# # max_score = 0
+# # for score in student_scores:
+# #     if score > max_score:
+# #         max_score = score
+# # print(max_score)
+
+# new_score = [8, 65, 89, 86, 55, 91, 64, 89]
+
+# max_score = 0
+# for score in new_score:
+#     if score > max_score:
+#         max_score = score
+# print(max_score)
+
+# # #range and using loops.
+# #
+# # for number in range(1, 11, 2):
+# #     print(number)
+
+# total = 0
+# for number in range (1,101):
+#     total += number
+# print(total)
+
+# for number in range (1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
+
+
