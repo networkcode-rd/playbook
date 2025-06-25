3bd2637d7626dfc9f20b6def53e65b7f32ba0ac3
Ratnavo Dutta <networkcode-rd@gmail.com>
2025-06-25 15:54:03 +0530

    Initial commit

--- ARM/templates/setup.ps1
+++
@@ -1,3 +0,0 @@
-import-module servermanager
-add-windowsfeature web-server -includeallsubfeature
-set-content -path "C:\inetpub\wwwroot\Default.html" -Value "This is the server $($env:computername) !"
